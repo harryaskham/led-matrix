8d368544fbaa7a20644a7581a8ad79226ee3ebdd
Harry Askham <harryaskham@gmail.com>
2015-12-03 19:29:47 +0000

    Adds the lights 32x32 file

---
+++ lights.py
@@ -0,0 +1,110 @@
+"""Modification of the sparkfruit example python to support 32x32 and D pin."""
+
+import RPi.GPIO as GPIO
+import time
+import threading
+
+GPIO.setmode(GPIO.BCM)
+red1_pin = 11
+green1_pin = 27
+blue1_pin = 7
+red2_pin = 8
+green2_pin = 9
+blue2_pin = 10
+clock_pin = 17
+a_pin = 22
+b_pin = 23
+c_pin = 24
+d_pin = 25
+latch_pin = 4
+oe_pin = 18
+
+GPIO.setup(red1_pin, GPIO.OUT)
+GPIO.setup(green1_pin, GPIO.OUT)
+GPIO.setup(blue1_pin, GPIO.OUT)
+GPIO.setup(red2_pin, GPIO.OUT)
+GPIO.setup(green2_pin, GPIO.OUT)
+GPIO.setup(blue2_pin, GPIO.OUT)
+GPIO.setup(clock_pin, GPIO.OUT)
+GPIO.setup(a_pin, GPIO.OUT)
+GPIO.setup(b_pin, GPIO.OUT)
+GPIO.setup(c_pin, GPIO.OUT)
+GPIO.setup(d_pin, GPIO.OUT)
+GPIO.setup(latch_pin, GPIO.OUT)
+GPIO.setup(oe_pin, GPIO.OUT)
+
+screen = [[0 for x in xrange(32)] for x in xrange(32)]
+
+delay = 0.0000001
+
+def clock():
+    GPIO.output(clock_pin, 1)
+    GPIO.output(clock_pin, 0)
+
+def latch():
+    GPIO.output(latch_pin, 1)
+    GPIO.output(latch_pin, 0)
+
+def bits_from_int(x):
+    a_bit = x & 1
+    b_bit = x & 2
+    c_bit = x & 4
+    d_bit = x & 8
+    return (a_bit, b_bit, c_bit, d_bit)
+
+def set_row(row):
+    a_bit, b_bit, c_bit, d_bit = bits_from_int(row)
+    GPIO.output(a_pin, a_bit)
+    GPIO.output(b_pin, b_bit)
+    GPIO.output(c_pin, c_bit)
+    GPIO.output(d_pin, d_bit)
+
+def set_color_top(color):
+    red, green, blue, _ = bits_from_int(color)
+    GPIO.output(red1_pin, red)
+    GPIO.output(green1_pin, green)
+    GPIO.output(blue1_pin, blue)
+
+def set_color_bottom(color):
+    red, green, blue, _ = bits_from_int(color)
+    GPIO.output(red2_pin, red)
+    GPIO.output(green2_pin, green)
+    GPIO.output(blue2_pin, blue)
+
+def refresh():
+    for row in range(16):
+        GPIO.output(oe_pin, 1)
+        set_color_top(0)
+        set_row(row)
+        for col in range(32):
+            set_color_top(screen[row][col])
+            set_color_bottom(screen[row+8][col])
+            clock()
+        #GPIO.output(oe_pin, 0)
+        latch()
+        GPIO.output(oe_pin, 0)
+        time.sleep(delay)
+
+def fill_rectangle(x1, y1, x2, y2, color):
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            screen[y][x] = color
+
+
+def set_pixel(x, y, color):
+    screen[y][x] = color
+"""
+def update():
+    t = 0
+    while True:
+        fill_rectangle(0, 0, 32, 32, t)
+        t += 1
+        time.sleep(1)
+        print screen
+th = threading.Thread(target=update)
+th.start()
+"""
+fill_rectangle(0, 0, 32, 32, 1)
+
+while True:
+    refresh()
