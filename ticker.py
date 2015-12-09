492c04025af1e1ab044be50d34cd684d4a57e386
Harry Askham <harryaskham@gmail.com>
2015-12-09 10:37:52 +0000

    sin mode

--- ticker.py
+++ ticker.py
@@ -6,26 +6,38 @@ import threading
 import inputs
 
 class Ticker(object):
-  
+
   PIN = 19
   SENSITIVITY = 0.9
 
-  def __init__(self):
+  class Mode(object):
+    LINEAR = 1
+    SIN = 2
+
+  def __init__(self, mode=Mode.LINEAR):
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(self.PIN, GPIO.OUT)
     self.sleep = 0.3
+    self.mode = mode
     self.pause = False
+
+  def _GetSleepTime(self, t):
+    if self.mode == Mode.LINEAR:
+      return self.sleep
+    if self.mode == Mode.SIN:
+      return self.sleep * 0.5 * (math.sin(t / 10.0) + 1 + self.sleep)
 
   def Start(self):
     def run():
+      t = 0
       while True:
         while self.pause:
           time.sleep(1)
         GPIO.output(self.PIN, True)
-        time.sleep(self.sleep)
+        time.sleep(self._GetSleepTime(t))
         GPIO.output(self.PIN, False)
-        time.sleep(self.sleep)
+        time.sleep(self._GetSleepTime(t))
 
     t = threading.Thread(target=run)
     t.daemon = True
@@ -44,7 +56,7 @@ class Ticker(object):
 
 
 
-ticker = Ticker()
+ticker = Ticker(mode=Ticker.Mode.SIN)
 rot = inputs.RotaryEncoder(on_rotation=ticker.HandleSpeed, on_button=ticker.Pause)
 
 ticker.Start()
